d01dd2a009ad6d94e3d3186b89eb337e49baed64
viveksriram1 <vivek.sriram@kiprosh.com>
2016-09-12 14:09:07 +0530

    style change

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :image, presence: true 
+  validates :title, presence: true
+  validates :description, presence: true 
 end
